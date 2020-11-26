85ef3ed84382bc42e2548cb01cb0655b318e5df1
n0k0m3 <vmnguyen251@gmail.com>
2020-11-26 10:59:00 -0500

    circumvent rate limit

--- jikan.py
+++ jikan.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pickle
-from time import time,sleep
+from time import time, sleep
 import os
 from tqdm import tqdm
 import pandas as pd
@@ -9,30 +9,30 @@ import pandas as pd
 if not os.path.isfile("mal_id_list_top_500.pkl"):
     mal_id_list = []
 
-    url="https://api.jikan.moe/v3/top/anime/{}/bypopularity"
+    url = "http://localhost:8000/v3/top/anime/{}/bypopularity"
     for i in tqdm(range(10)):
-        timeend = time()+4 # Rate Limit
+        #timeend = time()+4  # Rate Limit
 
-        response = requests.get(url.format(str(i)))
-        results=response.json()["top"]
+        response = requests.get(url.format(str(i+1)))
+        results = response.json()["top"]
         response.close()
 
         for res in results:
             mal_id = res["mal_id"]
             mal_id_list.append(mal_id)
 
-        if timeend-time()>0:
-            sleep(timeend-time()) # Rate Limit
+        # if timeend-time() > 0:
+        #     sleep(timeend-time())  # Rate Limit
 
-    with open("mal_id_list_top_500.pkl","wb") as f:
-        pickle.dump(mal_id_list,f)
+    with open("mal_id_list_top_500.pkl", "wb") as f:
+        pickle.dump(mal_id_list, f)
 
-with open("mal_id_list_top_500.pkl","rb") as f:
-    mal_id_list=pickle.load(f)
+with open("mal_id_list_top_500.pkl", "rb") as f:
+    mal_id_list = pickle.load(f)
 
-"""   
+"""
 "reviewer": {
-    ...     
+    ...
     "scores": {
             "overall": 8,
             "story": 8,
@@ -44,33 +44,47 @@ with open("mal_id_list_top_500.pkl","rb") as f:
 },
 "content": "review text"
 """
+data = [[0, 0, 0, 0, 0, 0, "test review"]]
+col = ['overall', 'story', 'animation',
+    'sound', 'character', 'enjoyment', 'review']
+data = pd.DataFrame(data, columns=col)
+num = ['overall', 'story', 'animation', 'sound', 'character', 'enjoyment']
 
-data = [[0,0,0,0,0,0,"test review"]]
-col = ['overall', 'story','animation','sound','character','enjoyment','review']
-data = pd.DataFrame(data, columns = col) 
-num = ['overall', 'story','animation','sound','character','enjoyment']
+url = "http://localhost:8000/v3/anime/{}/reviews/{}"
 
-url="https://api.jikan.moe/v3/anime/{}/reviews/{}"
-for mal_id in tqdm(mal_id_list):
-    print("\nWorking on MAL ID:",mal_id)
-    for i in range(100):
-        timeend = time()+4 # Rate Limit
+def bulk():
+    global data,col,num,url
+    for mal_id in tqdm(mal_id_list):
+        print("\nWorking on MAL ID:", mal_id)
+        for i in range(100):
+            # timeend = time()+0.25  # Rate Limit
+            while True:
+                response = requests.get(url.format(str(mal_id), str(i+1)))
+                try:
+                    reviews = response.json()["reviews"]
+                except KeyError:
+                    input("Maybe rate limited, check MAL again")
+                    continue
+                response.close()
+                break
 
-        response = requests.get(url.format(str(mal_id),str(i)))
-        reviews = response.json()["reviews"]
-        response.close()
+            if reviews == []:
+                break
 
-        if reviews==[]:
-            break
+            for r in reviews:
+                score_data = [s for s in r["reviewer"]["scores"].values()]
+                score_data.append(r["content"])
+                df = pd.DataFrame([score_data], columns=col)
+                data = data.append(df, ignore_index=True)
+                data[num] = data[num].astype("int8")
 
-        for r in reviews:
-            score_data = [s for s in r["reviewer"]["scores"].values()]
-            score_data.append(r["content"])
-            df = pd.DataFrame([score_data],columns=col)
-            data=data.append(df,ignore_index=True)
-            data[num]=data[num].astype("int8")
-        
-        data.to_hdf('data.h5', key='df', mode='w')
+            # if timeend-time()>0:
+            #     sleep(timeend-time()) # Rate Limit
+    return data
 
-        if timeend-time()>0:
-            sleep(timeend-time()) # Rate Limit
+try:
+    data=bulk()
+except KeyboardInterrupt:
+    raise
+finally:
+    data.to_hdf('data.h5', key='df', mode='w')
